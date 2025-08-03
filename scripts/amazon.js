e3bc5f63092045f82bf4abda04e2953a04f2aac6
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-08-03 07:46:41 +0600

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,6 +76,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             });
         };
 
-        console.log(cart);
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
